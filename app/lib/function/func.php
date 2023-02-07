d0ca11cbfa2f82c2e22f16149ce7e9aba526c597
I-Jurij <yjurij@gmail.com>
2023-02-07 12:30:13 +0300

    add

--- app/lib/function/func.php
+++ app/lib/function/func.php
@@ -119,5 +119,29 @@ function mb_ucfirst($string, $encoding)
     $then = mb_substr($string, 1, null, $encoding);
     return mb_strtoupper($firstChar, $encoding) . $then;
 }
-
+/**
+* function for url validation
+* @param string $url
+* @return bool
+*/
+function getResponseCode($url) {
+  $header = '';
+  $options = array(
+      CURLOPT_URL => trim($url),
+      CURLOPT_HEADER => false,
+      CURLOPT_RETURNTRANSFER => true
+  );
+  $ch = curl_init();
+  curl_setopt_array($ch, $options);
+  curl_exec($ch);
+  if (!curl_errno($ch)) {
+    $header = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  }
+  curl_close($ch);
+  if ($header > 0 && $header < 400) {
+    return true;
+  } else {
+      return false;
+  }
+}
 ?>
