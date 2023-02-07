d3bbf8fbc3f46eb2c9979d15c75eace9fb0feea2
I-Jurij <yjurij@gmail.com>
2023-02-07 18:10:45 +0300

    smch

--- app/models/home.php
+++ app/models/home.php
@@ -53,11 +53,19 @@ class Home
 		}
 
 		//get page list from db
-		if (null !== \App\Lib\Registry::get('page_list')) {
-			$this->data['page_list'] = \App\Lib\Registry::get('page_list');
+		if (null !== Registry::get('page_list')) {
+			$this->data['page_list'] = Registry::get('page_list');
 		} else {
-			$this->data['page_list'] = $this->db->db->select($this->table, "*");
-			\App\Lib\Registry::set('page_list', $this->data['page_list']);
+			$this->data['page_list'] = $this->db->db->select($this->table, [
+				"page_id", 
+				"page_alias", 
+				"page_title", 
+				"page_meta_description",
+				"page_h1",
+				"page_access",
+				"page_admin"
+			]);
+			Registry::set('page_list', $this->data['page_list']);
 		}
 		
 		//add css for head in template
