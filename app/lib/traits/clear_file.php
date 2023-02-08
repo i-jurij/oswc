602c55af1ef7fd69c1eb60ffdcbaa3d731556a7d
I-Jurij <yjurij@gmail.com>
2023-02-08 16:02:47 +0300

    smch

--- app/lib/traits/clear_file.php
+++ app/lib/traits/clear_file.php
@@ -5,9 +5,10 @@ trait Clear_file
 {
     /**
      * @param string $log_folder - path to logs folder
-     * @param int $keep_num_lines - number of rows to keep
+     * @param int $keep_num_lines - the number of lines to save, starting from the end of the file eg 33
+     * @return bool
      */
-    public static function clearfile($log_folder, $keep_num_lines) {
+    public static function clearfile($log_folder, $keep_num_lines = 0) {
         // clear log file if filetime > 1 week, but leave the last seven lines
         if (file_exists($log_folder)) {
             foreach (new \DirectoryIterator($log_folder) as $fileInfo) {
@@ -16,7 +17,7 @@ trait Clear_file
                 }
                 if ($fileInfo->isFile()) {
                     $lines = file($fileInfo->getPathname()); // reads the file into an array by line
-                    $keep = array_slice($lines,-40); // keep the last 40 elements of the array start at end
+                    $keep = (!empty($keep_num_lines)) ? array_slice($lines,-$keep_num_lines) : '';
                     if (file_put_contents($fileInfo->getPathname(), $keep) === false) {
                         return false;
                     } else {
@@ -24,8 +25,6 @@ trait Clear_file
                     }
                 }
             }
-        } else {
-            return false;
         }
     }
 }
