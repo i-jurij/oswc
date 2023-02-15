b31b394e664d523011a89d5cbe8a562f55e4ba8e
I-Jurij <yjurij@gmail.com>
2023-02-15 20:16:08 +0300

    add

--- app/lib/function/func.php
+++ app/lib/function/func.php
@@ -24,6 +24,7 @@ function files_in_dir($path, $ext = '')
         $arr = explode(',', $ext);
         foreach ($arr as $value) {
           $extt = mb_strtolower(trim($value));
+          //$extt = mb_strtolower(ltrim(trim($value), '.'));
           /*
           if(preg_match("/\.($extt)/", $file)) {
             $files[] = $file;
@@ -35,6 +36,32 @@ function files_in_dir($path, $ext = '')
         }
       }
     }
+  }
+  return $files;
+}
+/**
+ * @param string $dir - dir for scan
+ * @param string $ext - extension of files eg 'png' or 'png, webp, jpg'
+ * @return array basename  of files or false
+ */
+function filesindir($dir, $ext = '') {
+  if (file_exists($dir) && is_dir($dir) && is_readable($dir)) {
+    foreach (new DirectoryIterator($dir) as $fileInfo) {
+      if($fileInfo->isDot()) continue;
+      if (empty($ext)) {
+        $files[] = $fileInfo->getBasename();
+      } else {
+        $arr = explode(',', $ext);
+        foreach ($arr as $value) {
+          $extt = mb_strtolower(ltrim(trim($value), '.'));
+          if ($extt === $fileInfo->getExtension() ) {
+            $files[] = $fileInfo->getBasename();
+          }
+        }
+      }
+    }
+  } else {
+    return false;
   }
   return $files;
 }
