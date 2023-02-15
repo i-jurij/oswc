e62aafe2229fc256fa072c739820bafda19a04db
I-Jurij <yjurij@gmail.com>
2023-02-15 20:33:22 +0300

    add

--- app/lib/function/func.php
+++ app/lib/function/func.php
@@ -316,4 +316,10 @@ function del_empty_dir($dir) {
     }
   }
 }
+
+function human_filesize($bytes, $decimals = 2) {
+  $sz = 'BKMGTP';
+  $factor = floor((strlen($bytes) - 1) / 3);
+  return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+}
 ?>
